8fa7a801017f9e0ec06b182e158df5033a2dbbe4
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-04 10:29:54 -0400

    add linting and config

---
+++ .eslintrc.cjs
@@ -0,0 +1,63 @@
+const importSortingRules = {
+  'sort-imports': [
+    'warn',
+    {
+      // sort-imports can't sort import lines so we'll use eslint-plugin-import instead
+      ignoreDeclarationSort: true,
+    },
+  ],
+  'import/order': [
+    'warn',
+    {
+      groups: [
+        'builtin', // built-in imports go first
+        'external', // then external imports
+        'internal', // then absolute imports
+        ['sibling', 'parent'], // then relative imports (siblings and parents can go together)
+        'index', // then index imports
+        'unknown', // then everything else
+      ],
+      'newlines-between': 'always',
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true,
+      },
+    },
+  ],
+};
+
+const defaultRules = {
+  '@typescript-eslint/no-unused-vars': [
+    'warn',
+    { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+  ],
+  ...importSortingRules,
+};
+
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['**/*.test.*', '**/*.e2e.*'],
+      rules: {
+        ...defaultRules,
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  root: true,
+  rules: defaultRules,
+};
