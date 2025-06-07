e1a7f53116d55e566bda9abedf09f123925f7748
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-07 14:26:14 -0400

    use cumulative lap times

--- src/exercises/elimination-order.ts
+++ src/exercises/elimination-order.ts
@@ -15,20 +15,30 @@ const parseDriverTime = (value: string): DriverTime => {
 
 export function getEliminationOrder(laps: string[][]) {
   const eliminationOrder = [] as string[];
+  const driverTimesCumulative = {} as { [driver: string]: number };
 
+  // find the largest time, excluding drivers that have already been eliminated
   laps.map((driverTimes) => {
-    let lastPlace: DriverTime | undefined;
-
-    // find the largest time, excluding drivers that have already been eliminated
     driverTimes.map((driverTime) => {
-      const currentDriverTime = parseDriverTime(driverTime);
-      if (!eliminationOrder.includes(currentDriverTime.driver)) {
-        if (!lastPlace || lastPlace.time < currentDriverTime.time) {
-          lastPlace = currentDriverTime;
+      const { driver, time } = parseDriverTime(driverTime);
+      if (!eliminationOrder.includes(driver)) {
+        if (!(driver in driverTimesCumulative)) {
+          driverTimesCumulative[driver] = time;
+        } else {
+          driverTimesCumulative[driver] += time;
         }
       }
     });
 
+    // eliminate slowest driver
+    let lastPlace: DriverTime | undefined;
+    Object.entries(driverTimesCumulative).map(([driver, time]) => {
+      if (!eliminationOrder.includes(driver)) {
+        if (!lastPlace || lastPlace.time < time) {
+          lastPlace = { driver, time };
+        }
+      }
+    });
     if (lastPlace !== undefined) eliminationOrder.push(lastPlace.driver);
   });
 
