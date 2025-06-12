093bc77468109d265d03ac94409804ebf1b3a7c1
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-12 19:52:36 -0400

    refactor comments

--- src/exercises/console-app.test.ts
+++ src/exercises/console-app.test.ts
@@ -7,6 +7,43 @@ import {
   printTodo,
 } from '@/exercises/console-app';
 
+// ========================================================================
+
+// EXAMPLES:
+
+//     Please enter a command:
+//     > m foo - wash the dishes
+
+//     Please enter a command:
+//     > p foo
+//     foo - wash the dishes
+
+//     Please enter a command:
+//     > m foo - wash the dishes with hot water
+
+//     Please enter a command:
+//     > p foo
+//     foo - wash the dishes with hot water
+
+//     Please enter a command:
+//     > m bar - do the laundry
+
+//     Please enter a command:
+//     > p
+//     foo - wash the dishes with hot water
+//     bar - do the laundry
+
+//     Please enter a command:
+//     > d foo
+
+//     Please enter a command:
+//     > p
+//     bar - do the laundry
+
+//     Please enter a command:
+//     > q
+//     Exited Successfully!
+
 test.describe('console-app', () => {
   test('create todo', () => {
     createOrModify('foo - wash the dishes');
@@ -25,5 +62,12 @@ test.describe('console-app', () => {
       { id: 'foo', value: 'wash the dishes again' },
       { id: 'baz', value: 'write more todos' },
     ]);
+  });
+
+  test('delete todo', () => {
+    createOrModify('foo - wash the dishes');
+    createOrModify('baz - write more todos');
+    const todos = deleteTodo('baz');
+    expect(todos).toEqual([{ id: 'foo', value: 'wash the dishes' }]);
   });
 });
--- src/exercises/console-app.ts
+++ src/exercises/console-app.ts
@@ -11,17 +11,19 @@ let todos: Todo[] = [];
 //     Modifies an existing todo value or creates a new todo if no value exists for
 //     the given id.
 //     Note: everything after the ' - ' is the todo value excluding the first space.
-function createOrModify(input: string) {
-  const [id, value]  = input.split(' - ');
+export function createOrModify(input?: string) {
+  if (input === undefined) return todos; // fail silently
+  const [id, value] = input.split(' - ');
   const currentTodo = todos.filter((t) => t.id === id)?.[0];
   if (!currentTodo) {
-    todos.push({id, value});
+    todos.push({ id, value });
   } else {
-    console.log('modify!')
+    console.log('modify!');
     todos = todos.map((todo) => {
-      if(todo.id === id) {
+      if (todo.id === id) {
         return {
-          id, value
+          id,
+          value,
         };
       }
       return todo;
@@ -30,21 +32,14 @@ function createOrModify(input: string) {
   return todos;
 }
 
-// m foo - wash the dishes
-// const input = 'foo - wash the dishes';
-// const actual = createOrModify(input);
-// const expected = [{id: 'foo', value: 'wash the dishes'}];
-createOrModify('foo - wash the dishes');
-createOrModify('baz - write more todos');
-console.log(createOrModify('foo - wash the dishes again'));
-
-
-function printTodo(id: string): void;
-function printTodo(): void;
 // Print Todo(s):
 //     p <id>
 //     Print a todo specified by id, format: <id> - <todo>
-function printTodo(id?: string | undefined): void {
+//     The p command without an id will print all todos in the format: <id> - <todo>
+//     Note: the print order does not matter
+export function printTodo(id: string): void;
+export function printTodo(): void;
+export function printTodo(id?: string | undefined): void {
   const format = (todo: Todo) => `${todo.id} - ${todo.value}`;
   if (id) {
     const currentTodo = todos.filter((t) => t.id === id)?.[0];
@@ -59,106 +54,36 @@ function printTodo(id?: string | undefined): void {
     });
   }
 }
-printTodo('foo');
-printTodo('bar');
-
-//     p
-//     The p command without an id will print all todos in the format: <id> - <todo>
-//     Note: the print order does not matter
-// function printTodo() {}
-console.log('printing all todos: ');
-printTodo();
 
 // Delete Todo:
 //     d <id>
 //     Deletes the item
-function deleteTodo(id: string) {
+export function deleteTodo(id?: string) {
+  if (id === undefined) return; // fail silently
   todos = todos.filter((todo) => todo.id !== id);
+  return todos;
 }
-deleteTodo('baz');
-printTodo();
 
 // Quit:
 //     q
 //     Quits the program
-function quitTheProgram() {}
+export function quitTheProgram() {}
 
-
-
-
-// Your last Plain Text code is saved below:
 // Create a console application that allows the user to manage a todo list.
 // A todo list is an unordered list of items where each item has a string
 // id and a string value. For purposes of this exercise the todo list does not
 // need to be persisted after program exit.
+export function commandHandler(command: string, input?: string) {
+  const commands = {
+    m: { action: createOrModify, label: 'modify/create todo' },
+    p: { action: printTodo, label: 'print todo(s)' },
+    d: { action: deleteTodo, label: 'delete todo' },
+    q: { action: quitTheProgram, label: 'quit' },
+  } as const;
 
-
-// The following commands are supported:
-// - modify/create todo
-// - print todo(s)
-// - delete todo
-// - quit
-
-// ========================================================================
-
-// Commands:
-
-// Modify Todo:
-//     m <id> - <todo value>
-//     Modifies an existing todo value or creates a new todo if no value exists for
-//     the given id.
-//     Note: everything after the ' - ' is the todo value excluding the first space.
-
-// Print Todo(s):
-//     p <id>
-//     Print a todo specified by id, format: <id> - <todo>
-
-//     p
-//     The p command without an id will print all todos in the format: <id> - <todo>
-//     Note: the print order does not matter
-
-// Delete Todo:
-//     d <id>
-//     Deletes the item
-
-// Quit:
-//     q
-//     Quits the program
-
-
-// ========================================================================
-
-// EXAMPLES:
-
-//     Please enter a command:
-//     > m foo - wash the dishes
-
-//     Please enter a command:
-//     > p foo
-//     foo - wash the dishes
-
-//     Please enter a command:
-//     > m foo - wash the dishes with hot water
-
-//     Please enter a command:
-//     > p foo
-//     foo - wash the dishes with hot water
-
-//     Please enter a command:
-//     > m bar - do the laundry
-
-//     Please enter a command:
-//     > p
-//     foo - wash the dishes with hot water
-//     bar - do the laundry
-
-//     Please enter a command:
-//     > d foo
-
-//     Please enter a command:
-//     > p
-//     bar - do the laundry
-
-//     Please enter a command:
-//     > q
-//     Exited Successfully!
+  if (command in commands) {
+    commands[command as keyof typeof commands].action(input);
+  } else {
+    console.warn(`unsupported command '${command}'`);
+  }
+}
